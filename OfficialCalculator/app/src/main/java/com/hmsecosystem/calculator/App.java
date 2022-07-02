f08a579d0e93a5ed048a0f2bbc8d95c1d5d379d8
Vanh@FutureWei <vphomsav@futurewei.com>
2022-07-01 17:46:32 -0700

    add appsflyer SDK for attribution collection

--- OfficialCalculator/app/src/main/java/com/hmsecosystem/calculator/App.java
+++ OfficialCalculator/app/src/main/java/com/hmsecosystem/calculator/App.java
@@ -3,6 +3,8 @@ package com.hmsecosystem.calculator;
 import android.app.Application;
 import android.content.res.Resources;
 
+import com.appsflyer.AppsFlyerLib;
+
 public class App extends Application {
     private static Resources resources;
 
@@ -11,6 +13,17 @@ public class App extends Application {
         super.onCreate();
 
         resources = getResources();
+       final String AF_DEV_KEY = resources.getString(R.string.AF_DEV_KEY);
+
+        AppsFlyerLib.getInstance().init(AF_DEV_KEY,null,this);
+        AppsFlyerLib.getInstance().start(this);
+
+//        set single Dashboard third party store name here:
+//        The value set here appears in AppsFlyer raw data install_app_store field and in
+//        Overview dashboard
+//        Cohort dashboard
+//        Raw data reports (Raw data reports are an AppsFlyer premium feature)
+        AppsFlyerLib.getInstance().setOutOfStore("AG_Connect");
     }
 
     public static Resources getAppResources() {
